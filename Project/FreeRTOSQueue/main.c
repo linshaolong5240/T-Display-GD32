b9fdf07e82ada9e62cdc5b0316315aa1173c0f15
Sauron <634205468@qq.com>
2023-12-29 11:57:27 +0800

    update

--- Project/FreeRTOSQueue/main.c
+++ Project/FreeRTOSQueue/main.c
@@ -8,22 +8,20 @@
 #include "task.h"
 #include "queue.h"
 
-// 系统初始化函数使用标准工具链时没有在main函数前调用,声明后手动调用
-extern void _init();
-
-void TaskCreate(void);
-void TaskCreater(void *parameters);
-void TaskA(void *parameters);
-void TaskB(void *parameters);
-
-TaskHandle_t taskCreater = NULL;
-TaskHandle_t taskA = NULL;
-TaskHandle_t taskB = NULL;
-
 typedef struct
 {
     int value;
 } Message;
+
+// 系统初始化函数使用标准工具链时没有在main函数前调用,声明后手动调用
+extern void _init();
+
+void TaskCreate(void);
+void TaskA(void *parameters);
+void TaskB(void *parameters);
+
+TaskHandle_t taskA = NULL;
+TaskHandle_t taskB = NULL;
 
 QueueHandle_t taskAMessageQueue = NULL;
 
@@ -33,12 +31,6 @@ void IRQConfigure(void)
     eclic_global_interrupt_enable();                       // 使能全局中断
 }
 
-/*!
-    \brief      main function
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
 int main(void)
 {
     size_t size = 0;
@@ -48,53 +40,20 @@ int main(void)
     USARTInit();
     LEDInit();
 
-#if DEBUG
-    rtprintf("1\r\n");
-#endif
     TaskCreate();
-    // xTaskCreate(TaskCreater, "TaskCreater", 256, NULL, 2, &taskCreater);
-#if DEBUG
-    rtprintf("2\r\n");
-#endif
     vTaskStartScheduler();
-#if DEBUG
+
     while (1)
     {
         rtprintf("RTOS Exit\r\n");
     }
-#endif
+    return 0;
 }
-
-/* retarget the C library printf function to the USART */
-// int _put_char(int ch)
-// {
-//     usart_data_transmit(USART0, (uint8_t)ch);
-//     while (usart_flag_get(USART0, USART_FLAG_TBE) == RESET);
-//     return ch;
-// }
 
 void TaskCreate(void)
 {
-    size_t size = 0;
-    size = xPortGetFreeHeapSize();
-#if DEBUG
-    rtprintf("tc1 size %d\r\n", size);
-#endif
     xTaskCreate(TaskA, "TaskA", 256, NULL, 3, &taskA);
-    size = xPortGetFreeHeapSize();
-#if DEBUG
-    rtprintf("tc2 size %d\r\n", size);
-#endif
     xTaskCreate(TaskB, "TaskB", 256, NULL, 3, &taskB);
-}
-
-void TaskCreater(void *parameters)
-{
-    taskENTER_CRITICAL();
-    xTaskCreate(TaskA, "TaskA", 256, NULL, 2, &taskA);
-    xTaskCreate(TaskB, "TaskB", 256, NULL, 2, &taskB);
-    taskEXIT_CRITICAL();
-    vTaskDelete(taskCreater);
 }
 
 void TaskA(void *parameters)
@@ -113,13 +72,19 @@ void TaskA(void *parameters)
 #endif
                 LEDToggle(LED_R);
             }
+            else
+            {
+#if DEBUG
+                rtprintf("TaskA receive message failed");
+#endif
+            }
         }
     }
 }
 
 void TaskB(void *parameters)
 {
-    Message message = { 0 };
+    Message message = {0};
 
     while (1)
     {
@@ -136,6 +101,9 @@ void TaskB(void *parameters)
             }
             else
             {
+#if DEBUG
+                rtprintf("TaskB send message failed");
+#endif
             }
         }
     }
@@ -143,27 +111,34 @@ void TaskB(void *parameters)
 
 void freertos_risc_v_application_exception_handler(UBaseType_t mcause)
 {
-    // rtprintfISR("exception: 0x%04x\r\n", mcause);
-    // rtprintfISR("In trap handler, the mcause is %d\n", mcause);
-    // rtprintfISR("In trap handler, the mepc is 0x%x\n", read_csr(mepc));
-    // rtprintfISR("In trap handler, the mtval is 0x%x\n", read_csr(mbadaddr));
-    // _exit(mcause);
-    return 0;
+#if DEBUG_EXCEPTION
+    rtprintfISR("exception: 0x%04x\r\n", mcause);
+    rtprintfISR("In trap handler, the mcause is %d\n", mcause);
+    rtprintfISR("In trap handler, the mepc is 0x%x\n", read_csr(mepc));
+    rtprintfISR("In trap handler, the mtval is 0x%x\n", read_csr(mbadaddr));
+    _exit(mcause);
+#endif
 }
 
 void freertos_risc_v_application_interrupt_handler(UBaseType_t mcause)
 {
+#if DEBUG_INTERRUPT
     rtprintfISR("interrupt: 0x%04x\r\n", mcause);
+#endif
 }
 
 void vApplicationTickHook(void)
 {
-    // rtprintf("Tick\r\n");
+#if DEBUG_HOOK
+// rtprintf("Tick\r\n");
+#endif
 }
 
 void vApplicationIdleHook(void)
 {
-    // rtprintf("Idle\r\n");
+#if DEBUG_HOOK
+// rtprintf("Idle\r\n");
+#endif
 }
 
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
--- SDK/DEBUGConfig.h
+++ SDK/DEBUGConfig.h
@@ -2,6 +2,8 @@
 #define DEBUGCONFIG_H
 
 #define DEBUG 1
+#define DEBUG_EXCEPTION 0
+#define DEBUG_INTERRUPT 0
 #define DEBUG_HOOK 0
 #define DEBUG_TASK 1
 
